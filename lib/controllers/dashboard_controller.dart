c5864e1a9829409ec5db973eaf04297d6a21adc0
Onur Cevdet Güngören <onurcevdetgungoren@gmail.com>
2023-03-03 17:30:57 +0300

    feat: new feature screen and improvements

--- lib/controllers/dashboard_controller.dart
+++ lib/controllers/dashboard_controller.dart
@@ -22,17 +22,45 @@ class DashBoardController extends ChangeNotifier {
   List<DashboardElements>? _dashboardElements;
   List<DashboardElements>? get dashboardElements => _dashboardElements;
 
-  final List<DashboardElements>? _inProgressDashboardElements = [];
+  List<DashboardElements>? _inProgressDashboardElements = [];
   List<DashboardElements>? get inProgressDashboardElements =>
       _inProgressDashboardElements;
+
+  bool _inProgress = false;
+  bool get inProgress => _inProgress;
+  set inProgress(bool value) {
+    _inProgress = value;
+    notifyListeners();
+  }
+
+  List<DropdownMenuItem<String>> get dropdownItems {
+    List<DropdownMenuItem<String>> menuItems = [
+      const DropdownMenuItem(
+          child: Text('In Development'), value: 'In Development'),
+      const DropdownMenuItem(child: Text('Open'), value: "Open"),
+    ];
+    return menuItems;
+  }
+
+  String? _selectedDropdownItem;
+  String? get selectedDropdownItem => _selectedDropdownItem;
+  set selectedDropdownItem(String? value) {
+    _selectedDropdownItem = value;
+    notifyListeners();
+  }
 
   Future<List<DashboardElements?>> getDashboardElements() async {
     try {
       state = DashboardStates.loading;
-      var snapShot = await database.child('dashboardmodel').get();
+      _dashboardElements = [];
+      _inProgressDashboardElements = [];
+      _percentageSum = 0;
+      var snapShot = await database.child('dashboardmodel').once();
 
-      _dashboardElements = (snapShot.value as List)
-          .map((e) => DashboardElements.fromMap(e))
+      _dashboardElements = (snapShot.snapshot.value as Map<dynamic, dynamic>)
+          .values
+          .toList()
+          .map((e) => DashboardElements.fromMap(e as Map<dynamic, dynamic>))
           .toList();
 
       for (var element in _dashboardElements!) {
@@ -47,5 +75,36 @@ class DashBoardController extends ChangeNotifier {
     }
 
     return _dashboardElements!;
+  }
+
+  Future<void> addNewFeature(DashboardElements elements) async {
+    try {
+      state = DashboardStates.loading;
+      await FirebaseDatabase.instance
+          .ref('dashboardmodel')
+          .push()
+          .set(elements.toMap());
+      state = DashboardStates.loaded;
+    } catch (e) {
+      state = DashboardStates.error;
+    }
+  }
+
+  Future<void> updateFeature(DashboardElements elements) async {
+    try {
+      state = DashboardStates.loading;
+      var dataRef = FirebaseDatabase.instance.ref().child("dashboardmodel");
+      var snapshot = await dataRef
+          .orderByChild("featurename")
+          .equalTo(elements.featureName)
+          .once();
+
+      Map map = snapshot.snapshot.value as Map<dynamic, dynamic>;
+      String key = map.keys.first;
+      await dataRef.child(key).set(elements.toMap());
+      state = DashboardStates.loaded;
+    } catch (e) {
+      state = DashboardStates.error;
+    }
   }
 }
