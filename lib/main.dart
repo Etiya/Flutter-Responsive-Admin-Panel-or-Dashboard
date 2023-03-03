4f0dddb06437c3a914ea4a1dd1fa4d6079c13e2e
Onur Cevdet Güngören <onurcevdetgungoren@gmail.com>
2023-03-03 17:26:20 +0300

    feat: screenUtil added to pubspec.yaml and applied to project

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:admin/controllers/menu_controller.dart';
 import 'package:admin/screens/authentication/wrapper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
