84b4892f0f8ace7cb14ba38c7363754aed29a8a1
Necati Atamer Şahin <atamersahin1997@gmail.com>
2023-04-04 09:35:06 +0300

    Update profile_screen.dart

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -40,143 +40,146 @@ class ProfileScreenBody extends StatelessWidget {
         decoration: const BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(10)),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                GestureDetector(
-                  onTap: () async {
-                    await profileController.pickImage();
-                    await profileController.uploadImage();
-                    await profileController.updateProfilePhoto();
-                  },
-                  child: profileController.firebaseAuth.currentUser?.photoURL ==
-                          null
-                      ? CircleAvatar(
-                          radius: 30,
-                          child: Icon(
-                            Icons.person,
-                            size: 30.r,
-                          ),
-                        )
-                      : CircleAvatar(
-                          radius: 30,
-                          backgroundImage: NetworkImage(profileController
-                              .firebaseAuth.currentUser!.photoURL!),
-                        ),
-                ),
-                10.horizontalSpace,
-                Column(
-                  children: const [
-                    Text(
-                      'Change Profile Photo',
-                      style: TextStyle(fontWeight: FontWeight.w500),
-                    ),
-                    Text(
-                      'Max Upload Limit: 2MB',
-                      style: TextStyle(
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            20.verticalSpace,
-            Container(
-              padding: const EdgeInsets.all(defaultPadding),
-              decoration: const BoxDecoration(
-                color: secondaryColor,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  const Text('Name'),
-                  10.verticalSpace,
-                  TextFormField(
-                    readOnly: true,
-                    initialValue:
-                        profileController.firebaseAuth.currentUser?.displayName,
-                    decoration: InputDecoration(
-                        fillColor: bgColor,
-                        filled: true,
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.r)))),
-                    onChanged: (value) {},
+                  GestureDetector(
+                    onTap: () async {
+                      await profileController.pickImage();
+                      await profileController.uploadImage();
+                      await profileController.updateProfilePhoto();
+                    },
+                    child:
+                        profileController.firebaseAuth.currentUser?.photoURL ==
+                                null
+                            ? CircleAvatar(
+                                radius: 30,
+                                child: Icon(
+                                  Icons.person,
+                                  size: 30.r,
+                                ),
+                              )
+                            : CircleAvatar(
+                                radius: 30,
+                                backgroundImage: NetworkImage(profileController
+                                    .firebaseAuth.currentUser!.photoURL!),
+                              ),
                   ),
-                  20.verticalSpace,
-                  const Text('Surname'),
-                  10.verticalSpace,
-                  TextFormField(
-                    readOnly: true,
-                    initialValue:
-                        profileController.firebaseAuth.currentUser?.displayName,
-                    decoration: InputDecoration(
-                        fillColor: bgColor,
-                        filled: true,
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.r)))),
-                    onChanged: (value) {},
+                  10.horizontalSpace,
+                  Column(
+                    children: const [
+                      Text(
+                        'Change Profile Photo',
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        'Max Upload Limit: 2MB',
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
                   ),
-                  20.verticalSpace,
-                  const Text('Phone Number'),
-                  10.verticalSpace,
-                  TextFormField(
-                    readOnly: true,
-                    initialValue:
-                        profileController.firebaseAuth.currentUser?.phoneNumber,
-                    decoration: InputDecoration(
-                        fillColor: bgColor,
-                        filled: true,
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.r)))),
-                    onChanged: (value) {},
-                  ),
-                  20.verticalSpace,
-                  const Text('Mail'),
-                  10.verticalSpace,
-                  TextFormField(
-                    controller: mailController,
-                    decoration: InputDecoration(
-                        fillColor: bgColor,
-                        filled: true,
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.r)))),
-                    onChanged: (value) {},
-                  ),
-                  10.verticalSpace,
-                  // const Align(
-                  //   alignment: Alignment.centerRight,
-                  //   child: Text(
-                  //     'Want to change your pass?',style: ,
-                  //   ),
-                  // ),
                 ],
               ),
-            ),
-            10.verticalSpace,
-            Center(
-              child: ElevatedButton(
-                onPressed: () async {
-                  await profileController.updateMail(mailController!.text);
-                },
-                child: const Text('Save'),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(secondaryColor),
-                  padding: MaterialStatePropertyAll(
-                      EdgeInsets.symmetric(horizontal: 40.r, vertical: 10.r)),
+              20.verticalSpace,
+              Container(
+                padding: const EdgeInsets.all(defaultPadding),
+                decoration: const BoxDecoration(
+                  color: secondaryColor,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Name'),
+                    10.verticalSpace,
+                    TextFormField(
+                      readOnly: true,
+                      initialValue: profileController
+                          .firebaseAuth.currentUser?.displayName,
+                      decoration: InputDecoration(
+                          fillColor: bgColor,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.r)))),
+                      onChanged: (value) {},
+                    ),
+                    20.verticalSpace,
+                    const Text('Surname'),
+                    10.verticalSpace,
+                    TextFormField(
+                      readOnly: true,
+                      initialValue: profileController
+                          .firebaseAuth.currentUser?.displayName,
+                      decoration: InputDecoration(
+                          fillColor: bgColor,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.r)))),
+                      onChanged: (value) {},
+                    ),
+                    20.verticalSpace,
+                    const Text('Phone Number'),
+                    10.verticalSpace,
+                    TextFormField(
+                      readOnly: true,
+                      initialValue: profileController
+                          .firebaseAuth.currentUser?.phoneNumber,
+                      decoration: InputDecoration(
+                          fillColor: bgColor,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.r)))),
+                      onChanged: (value) {},
+                    ),
+                    20.verticalSpace,
+                    const Text('Mail'),
+                    10.verticalSpace,
+                    TextFormField(
+                      controller: mailController,
+                      decoration: InputDecoration(
+                          fillColor: bgColor,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.r)))),
+                      onChanged: (value) {},
+                    ),
+                    10.verticalSpace,
+                    // const Align(
+                    //   alignment: Alignment.centerRight,
+                    //   child: Text(
+                    //     'Want to change your pass?',style: ,
+                    //   ),
+                    // ),
+                  ],
                 ),
               ),
-            )
-          ],
+              10.verticalSpace,
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    await profileController.updateMail(mailController!.text);
+                  },
+                  child: const Text('Save'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(secondaryColor),
+                    padding: MaterialStatePropertyAll(
+                        EdgeInsets.symmetric(horizontal: 40.r, vertical: 10.r)),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       );
     } else if (profileController.state == ProfileState.loading) {
